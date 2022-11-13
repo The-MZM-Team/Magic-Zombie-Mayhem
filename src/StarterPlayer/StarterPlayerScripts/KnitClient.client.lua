93f3f2c06b1bf7b89f7a2d77d1e89e69ac45ed3d
Ryan Chang <ryanchang12321@gmail.com>
2022-11-12 23:25:51 -0500

    Updated server and client

--- src/StarterPlayer/StarterPlayerScripts/KnitClient.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/KnitClient.client.lua
@@ -1,18 +1,1 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
-local Component = require(game:GetService("ReplicatedStorage").Packages.Component)
-
-local Controllers = ReplicatedStorage:WaitForChild("Controllers")
-local Components = ReplicatedStorage:WaitForChild("Components")
-
-local clockOffset = os.clock()
-
-Knit.AddControllers(Controllers)
-
-Knit.Start()
-	:andThen(function()
-		Component.Auto(Components)
-		print(string.format("[Client-Knit]: Framework Initiated [%sms]", os.clock() - clockOffset))
-	end)
-	:catch(warn)
+print("is the client!")
