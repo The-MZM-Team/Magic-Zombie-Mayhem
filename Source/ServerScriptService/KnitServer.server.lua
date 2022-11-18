c7b4d017d897e8831a10d57d4332894bf372142d
Ryan Chang <ryanchang12321@gmail.com>
2022-11-18 09:43:37 -0500

    Refactored controllers and services so it is centralized around Core

--- Source/ServerScriptService/KnitServer.server.lua
+++ Source/ServerScriptService/KnitServer.server.lua
@@ -1,13 +1,16 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
 
-local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
-local Component = require(game:GetService("ReplicatedStorage").Packages.Component)
+local Knit = require(ReplicatedStorage.Submodules.Packages.Knit)
+local Component = require(ReplicatedStorage.Submodules.Packages.Component)
 
 local Services = ServerScriptService:WaitForChild("Services")
+local CoreServices = ServerScriptService.Submodules.Core.Source.Services
 local Components = ServerScriptService:WaitForChild("Components")
 
 local clockOffset: number = os.clock()
 
+Knit.AddServices(CoreServices)
 Knit.AddServices(Services)
 
 Knit.Start()
--- Source/StarterPlayer/StarterPlayerScripts/KnitClient.client.lua
+++ Source/StarterPlayer/StarterPlayerScripts/KnitClient.client.lua
@@ -1,13 +1,15 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
-local Component = require(game:GetService("ReplicatedStorage").Packages.Component)
+local Knit = require(ReplicatedStorage.Submodules.Packages.Knit)
+local Component = require(ReplicatedStorage.Submodules.Packages.Component)
 
 local Controllers = ReplicatedStorage:WaitForChild("Controllers")
+local CoreControllers = ReplicatedStorage.Submodules.Core.Source.Controllers
 local Components = ReplicatedStorage:WaitForChild("Components")
 
-local clockOffset: number = os.clock()
+local clockOffset = os.clock()
 
+Knit.AddControllers(CoreControllers)
 Knit.AddControllers(Controllers)
 
 Knit.Start()
