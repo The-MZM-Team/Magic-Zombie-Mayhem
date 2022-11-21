d50f60d974df2e8f8bc8deb819a81f97a4e20114
Ryan Chang <ryanchang12321@gmail.com>
2022-11-21 00:38:41 -0500

    Updated WindController

--- Source/ReplicatedStorage/Controllers/WindController/init.lua
+++ Source/ReplicatedStorage/Controllers/WindController/init.lua
@@ -4,9 +4,11 @@ local WindShake = require(ReplicatedStorage.Submodules.Libraries.WindShake)
 local WindLines = require(ReplicatedStorage.Submodules.Libraries.WindShake.WindLines)
 local Knit = require(ReplicatedStorage.Submodules.Packages.Knit)
 
-local WIND_DIRECTION = Vector3.new(1, 0, 0.3)
-local WIND_SPEED = 25
-local WIND_POWER = 0.55
+local WIND_DIRECTION: Vector3 = Vector3.new(1, 0, 0.3)
+local WIND_SPEED: number = 25
+local WIND_POWER: number = 0.55
+local LINES_LIFETIME: number = 1.5
+local LINES_SPAWN_RATE: number = 5
 
 local WindShakeController = Knit.CreateController({
 	Name = "WindShakeController",
@@ -14,6 +16,13 @@ local WindShakeController = Knit.CreateController({
 })
 
 function WindShakeController:KnitInit()
+	local linesDefaultSettings = {
+		Direction = WIND_DIRECTION,
+		Speed = WIND_SPEED,
+		Lifetime = LINES_LIFETIME,
+		SpawnRate = LINES_SPAWN_RATE,
+	}
+
 	WindShake:SetDefaultSettings({
 		WindSpeed = WIND_SPEED,
 		WindDirection = WIND_DIRECTION,
@@ -21,13 +30,7 @@ function WindShakeController:KnitInit()
 	})
 
 	WindShake:Init() -- Anything with the WindShake tag will now shake
-
-	WindLines:Init({ -- Create Wind Lines
-		Direction = WIND_DIRECTION,
-		Speed = WIND_SPEED,
-		Lifetime = 1.5,
-		SpawnRate = 5,
-	})
+	WindLines:Init(linesDefaultSettings)
 end
 
 return WindShakeController
